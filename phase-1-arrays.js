e6ce82f5a47965de962e702eda63c9a3f4ff23ab
mxcrpntr <carpenter.max@gmail.com>
2023-06-20 16:06:25 -0400

    first commit

--- phase-1-arrays.js
+++ phase-1-arrays.js
@@ -1,1 +1,40 @@
 // Your code here
+
+// Array.prototype.uniq
+
+
+Array.prototype.uniq = function() {
+    let newArray = [];
+    this.forEach(function(el) {
+        if (!newArray.includes(el)) {
+            newArray.push(el);
+        }
+    });
+    return newArray;
+}
+
+// Array.prototype.twosum
+
+Array.prototype.twoSum = function() {
+    let newArray = [];
+    for (let i = 0 ; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length ; j++) {
+            if (this[i] + this[j] === 0) {
+                newArray.push([i,j]);
+            }
+        }
+    }
+    return newArray;
+}
+
+// Array.prototype.transpose
+
+Array.prototype.transpose = function() {
+    let newArray = Array.from(Array(this.length), () => []);
+    for (let i = 0 ; i < this.length; i++) {
+        for (let j = 0; j < this.length ; j++) {
+            newArray[j].push(this[i][j]);
+        }
+    }
+    return newArray;
+}
