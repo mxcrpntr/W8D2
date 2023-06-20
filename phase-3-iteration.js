bb7727ba95b3906b5c1f29ce40fbb20f55f9f01e
mxcrpntr <carpenter.max@gmail.com>
2023-06-20 17:34:45 -0400

    started on recursion

--- phase-3-iteration.js
+++ phase-3-iteration.js
@@ -1,1 +1,30 @@
 // Your code here
+
+
+// Array.prototype.bubbleSort
+
+Array.prototype.bubbleSort = function() {
+    let sorted = false;
+    while (sorted === false) {
+        sorted = true;
+        for (i = 0; i < this.length - 1; i++) {
+            if (this[i] > this[i+1]) {
+                sorted = false;
+                [this[i], this[i+1]] = [this[i+1], this[i]];
+            }
+        }
+    }
+    return this;
+}
+
+// String.prototype.substrings
+String.prototype.substrings = function() {
+    let newArray = [];
+    for (let i = 0 ; i < this.length - 1; i++) {
+        for (let j = i + 1; j < this.length ; j++) {
+            newArray.push(this.slice(i,j));
+        }
+    }
+    return newArray;
+}
+
--- phase-4-recursion.js
+++ phase-4-recursion.js
@@ -1,1 +1,9 @@
 // Your code here
+
+function range(start,end) {
+    newArray = [];
+    for (i = start; i <= end; i++) {
+        newArray.push(i);
+    }
+    return newArray;
+}
